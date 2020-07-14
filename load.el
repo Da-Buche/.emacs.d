5d9b6f6a5ea93ac877f5a4d937589cc390bf0ef2
Aurel Buche <aurelien.buchet@tutanota.com>
2020-07-14 14:33:05 +0200

    updated load.el

--- load.el
+++ load.el
@@ -1,26 +1,19 @@
 ;; Loading recursively all files from the current directory
 
-(let
-    ((l-files (cons (concat emacs-settings-root "/user") nil))
-     t-file
-     );def
-  (while
-      (setq t-file (pop l-files))
+(let* ((emacs-settings-root "~/.emacs.d")
+       (l-files (cons (concat emacs-settings-root "/user") nil))
+       t-file
+       );def
+  (while (setq t-file (pop l-files))
     ;; adding contained files to list if filename is a directory
-    (when
-        (and
-         (not (equal "." t-file))
-         (not (equal ".." t-file))
-         (file-directory-p t-file)
-         )
+    (when (and (not (equal "." t-file))
+	       (not (equal ".." t-file))
+	       (file-directory-p t-file)
+	       )
       (setq l-files (append l-files (directory-files t-file t "[^~.]$")))
       );when
-    (when
-        (and
-         (< 3 (length t-file))
-         (equal (substring t-file -3) ".el")
-         )
+    (when (and (< 3 (length t-file))
+	       (equal (substring t-file -3) ".el")
+	       )
       (load t-file)
-      );when
-    );while
-  );let
+      )))
